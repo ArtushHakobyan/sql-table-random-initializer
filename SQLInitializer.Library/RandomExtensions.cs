85887d223510647d51e7f927db692d376a4f471b
Artush Hakobyan <artush.hakobyan3@gmail.com>
2019-04-24 13:09:05 -0700

    fix #1

--- SQLInitializer.Library/RandomExtensions.cs
+++ SQLInitializer.Library/RandomExtensions.cs
@@ -25,7 +25,7 @@ namespace SQLInitializer.Library
 
         public static string NextDateTime(this Random rd)
         {
-            return $"'{rd.Next(1990, 2018)}-{rd.Next(0, 13)}-{rd.Next(0, 32)}', ";
+            return $"{rd.Next(1990, 2019)}-{rd.Next(0, 13)}-{rd.Next(0, 28)}";
         }
 
         public static Byte NextByte(this Random rd)
