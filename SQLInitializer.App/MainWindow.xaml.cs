18d911227084373f956287dba8b68ee857dea5ec
Artush Hakobyan <artush.hakobyan3@gmail.com>
2019-04-24 12:14:28 -0700

    add Initialize button functionality

--- SQLInitializer.App/MainWindow.xaml.cs
+++ SQLInitializer.App/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
 using System.Data.SqlClient;
+using SQLInitializer.Library;
 
 namespace SQLInitializer.App
 {
@@ -67,6 +68,97 @@ namespace SQLInitializer.App
 
                     throw;
                 }
+            }
+        }
+
+        private void BtnInitialize_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnectionStringBuilder builer = new SqlConnectionStringBuilder()
+            {
+                DataSource = txtDataSource.Text,
+                InitialCatalog = txtInitialCatalog.Text,
+                IntegratedSecurity = chkSecurity.IsChecked ?? false
+            };
+
+            string connectionString = builer.ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SQLInitializer.Library.Initializer.Initialize(int.Parse(txtCount.Text), connectionString, cmbTables.SelectedValue.ToString());
+                string cmdString = $"SELECT TOP(100) * FROM {cmbTables.SelectedItem.ToString()}";
+                SqlCommand command = new SqlCommand(cmdString, connection);
+                DataTable table = LoadTable(connectionString, cmbTables.SelectedValue.ToString());
+                grdResult.DataContext = table.DefaultView;
+
+                try
+                {
+                    connection.Open();
+
+                    //for (int i = 0; i < table.Rows.Count; i++)
+                    //{
+                    //    lstResult.Items.Add(table.Rows[i]);
+                    //}
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                    throw;
+                }
+            }
+        }
+
+        private void AddTableColumns(string connectionString, string table)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string cmdString =
+                    $"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = '{table.Substring(4)}'";
+                SqlCommand command = new SqlCommand(cmdString, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    var gridView = new GridView();
+
+                    while (reader.Read())
+                    {
+                        gridView.Columns.Add(new GridViewColumn { Header = reader[0].ToString() });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                    throw;
+                }
+            }
+        }
+
+        private DataTable LoadTable(string connectionString, string table)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string cmdString = $"SELECT * FROM {table}";
+                SqlCommand command = new SqlCommand(cmdString, connection);
+                DataTable tbl = new DataTable();
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    tbl.Load(reader);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                    throw;
+                }
+
+                return tbl;
             }
         }
     }
